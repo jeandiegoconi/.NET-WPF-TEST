58e7b71a8c461a30d22a368a88f1a16c9bc20062
jeandiegoconi <86448605+jeandiegoconi@users.noreply.github.com>
2022-11-30 21:41:33 -0300

    Pull

---
+++ Presentacion/Negocio/CategoriaBLL.cs
@@ -0,0 +1,57 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class CategoriaBLL
+    {
+
+        
+
+        TareasPendientesEntities tp = new TareasPendientesEntities();
+
+        public void Add(string nombre)
+        {
+            if (Get(nombre) == null)
+            {
+                Categoria nueva = new Categoria();
+                nueva.Nombre = nombre;
+
+                tp.Categoria.Add(nueva);
+                tp.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("Categoria ya existe.");
+            }
+        }
+
+        public Categoria Get(string nombre)
+        {
+            Categoria categoria = tp.Categoria.Where(c => c.Nombre == nombre).FirstOrDefault();
+
+            return categoria;
+        }
+
+        public List<Categoria> GetAll()
+        {
+            return tp.Categoria.ToList();
+        }
+
+        public void Delete(string nombre)
+        {
+            Categoria c = Get(nombre);
+
+            if (c != null)
+            {
+                tp.Categoria.Remove(c);
+                tp.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ Presentacion/Presentacion/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        CategoriaBLL catBLL = new CategoriaBLL();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListarCategorias();
+        }
+
+        private void ListarCategorias()
+        {
+            LstCategorias.ItemsSource = catBLL.GetAll();
+        }
+
+        private void BtnAddCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nombre = TxtNombre.Text.Trim();
+                catBLL.Add(nombre);
+
+                MessageBox.Show("Categoria agregada", "Nueva Categoria", MessageBoxButton.OK, MessageBoxImage.Information);
+                TxtNombre.Text = string.Empty;
+                ListarCategorias();
+            }
+            catch(ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Nueva Categoria", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnEliminarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = LstCategorias.SelectedItem.ToString();
+            catBLL.Delete(nombre);
+
+            ListarCategorias();
+        }
+    }
+}
