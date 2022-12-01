65ad18bd8dc383ed86b2b7ca06554a0a09d0542b
jeandiegoconi <86448605+jeandiegoconi@users.noreply.github.com>
2022-11-30 21:43:54 -0300

    xd

---
+++ Semana14/BLL/ClasificacionBLL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class ClasificacionBLL
+    {
+        ToDoListEntities bd = new ToDoListEntities();
+
+        //CRUD
+
+        public void Add(string nombre)
+        {
+            Clasificacion nueva = new Clasificacion();
+            nueva.Nombre = nombre;
+
+            bd.Clasificacion.Add(nueva);
+            bd.SaveChanges();
+        }
+
+        public List<Clasificacion> GetAll()
+        {
+
+            return bd.Clasificacion.OrderBy(c => c.Nombre).ToList();
+        }
+
+        public List<Clasificacion> Filter(string texto)
+        {
+            return bd.Clasificacion.Where(c => c.Nombre.Contains(texto)).ToList(); 
+        }
+        
+
+        public void Remove(string nombre)
+        {
+            //1. Obtener desde la bd el elemento a eliminar
+            Clasificacion clasificacion = this.Get(nombre);
+            //2. Eliminar el elemento de la bd
+            bd.Clasificacion.Remove(clasificacion);
+
+            //3. Guardar los cambios
+            bd.SaveChanges();
+        }
+
+        public void Update(Clasificacion clasificacion, string nuevoNombre)
+        {
+            //this.Get(clasificacion.Nombre).Nombre = nuevoNombre;
+
+            //1. Obtener el elemento a modificar en la BD
+            Clasificacion c = this.Get(clasificacion.Nombre);
+            //2. Realizar los cambios
+            c.Nombre = nuevoNombre;
+            //3.Guardar los cambios
+            bd.SaveChanges();
+        }
+
+        public Clasificacion Get(string nombre)
+        {
+            Clasificacion clasificacion = bd.Clasificacion.Where(c => c.Nombre == nombre).FirstOrDefault();
+            return clasificacion;
+        }
+    }
+}
---
+++ Semana14/PL/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BLL;
+using DAL;
+
+namespace PL
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClasificacionBLL cbll = new ClasificacionBLL();
+        Clasificacion clasificacion = new Clasificacion();
+        public MainWindow()
+        {
+            InitializeComponent();
+            lstClasificaciones.ItemsSource = cbll.GetAll();
+        }
+
+        private void BtnNuevaClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtClasificacion.Text;
+
+            //Validaciones de entrada
+
+            cbll.Add(nombre);
+
+            MessageBox.Show("Clasificación creada", "Nueva Clasificacion", MessageBoxButton.OK, MessageBoxImage.Information);
+            txtClasificacion.Text = string.Empty;
+
+            lstClasificaciones.ItemsSource = null;
+            lstClasificaciones.ItemsSource = cbll.GetAll();
+
+        }
+
+        private void BtnEliminarClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = lstClasificaciones.SelectedValue.ToString();
+            cbll.Remove(nombre);
+
+            lstClasificaciones.ItemsSource = null;
+            lstClasificaciones.ItemsSource = cbll.GetAll();
+
+        }
+
+        private void BtnEditarClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            btnNuevaClasificacion.Visibility = Visibility.Hidden;
+            btnCancelarEdicion.Visibility = Visibility.Visible;
+            btnGuardarClasificacion.Visibility = Visibility.Visible;
+
+            clasificacion = (Clasificacion)lstClasificaciones.SelectedItem;
+            //(RECORDAR VALIDAR QUE HAYA UN ELEMENTO SELECCIONADO)
+
+            txtClasificacion.Text = clasificacion.Nombre;
+
+
+        }
+
+        private void BtnCancelarEdicion_Click(object sender, RoutedEventArgs e)
+        {
+            btnNuevaClasificacion.Visibility = Visibility.Visible;
+            btnCancelarEdicion.Visibility = Visibility.Hidden;
+            btnGuardarClasificacion.Visibility = Visibility.Hidden;
+
+            txtClasificacion.Text = string.Empty;
+        }
+
+        private void BtnGuardarClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtClasificacion.Text.Trim();
+            cbll.Update(clasificacion, nombre);
+
+            lstClasificaciones.ItemsSource = null;
+            lstClasificaciones.ItemsSource = cbll.GetAll();
+
+            txtClasificacion.Text = string.Empty;
+
+            btnNuevaClasificacion.Visibility = Visibility.Visible;
+            btnCancelarEdicion.Visibility = Visibility.Hidden;
+            btnGuardarClasificacion.Visibility = Visibility.Hidden;
+
+
+        }
+
+        private void BtnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            string texto = txtFiltro.Text;
+            //VALIDACIONES
+
+            lstClasificaciones.ItemsSource = null;
+            lstClasificaciones.ItemsSource = cbll.Filter(texto);
+        }
+    }
+}
